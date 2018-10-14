edfc5d1e3df46ea77a2b879f94c85f3ba70484e4
Rinat <rinat.babichev@gmail.com>
2018-10-15 00:21:11 +0300

    Nth power problem solution

---
+++ SOLUTIONS/NThPower.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    res = 0
+    for number in range(0, 10 ** (n + 1)):
+        sum_of_digits = sum(int(digit) ** n for digit in str(number))
+        if sum_of_digits == number:
+            res += sum_of_digits
+    print(res)

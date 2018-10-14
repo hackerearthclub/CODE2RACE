b7b931bd99714587aa4129fd775fd5decd4d3755
Zhanet Ivanova <ivanova.zhanet@gmail.com>
2018-10-14 18:38:17 +0100

    Hello World class in Java

---
+++ SOLUTIONS/HelloWorldClass.java
@@ -0,0 +1,11 @@
+// This is my first attempt to write "Hello World!" in java 11
+
+public class HelloWorldClass
+{
+
+  public static void main(String[] args)
+  {
+    String newline = System.lineSeparator();
+    System.out.println("Hello, World!" + newline + "I hope you are well!");
+  }
+}

b6750fe476f99955279548cd9d2b815acc0f60e4
lux-ed <34897232+lux-ed@users.noreply.github.com>
2018-10-15 13:21:00 +0200

    Update rock_paper.py

--- SOLUTIONS/rock_paper.py
+++ SOLUTIONS/rock_paper.py
@@ -1,4 +1,4 @@
-import getpass  #library to hide the user's input
+import getpass  #to hide the user's input
 moves = {"rock":1 ,"paper":2,"scissors":3}
 def play(player):
     move = getpass.getpass("{}'s move:    ".format(player))
@@ -13,11 +13,11 @@ def compare(player1 ,player2):
         return True # Player 1 wins
     else:
         return False # Player 2 wins
-while input("Wanna Play ?   ").lower() in ["y","yes"]:
+while input("Wanna Play ?   ").lower() in ["y","yes","ye","yeah"]:
     player1 = play("player1")
     player2 = play("player2")
     if (player1==player2):
-        print("\nDraw \n")
+        print("\nIt's a tie \n")
     else:
         winner = "Player 1" if (compare(player1 ,player2)) else "Player 2"
         print(" \n{} won the game \n".format(winner))

f6e42b6c48b5e5d659812fb24fdaf88faab90c5c
Earthling <Earthling@EARTHLING-PC>
2018-10-15 20:58:14 -0700

    Accounted for outliers

--- SOLUTIONS/LengthofLargestSub_Array.py
+++ SOLUTIONS/LengthofLargestSub_Array.py
@@ -2,6 +2,8 @@ import math
 
 arr0 = [4,7,4,7,11,5,4,4,4,5]
 arr1 = [1,9,3,4,5,6,7,8]
+arr2 = [2,3,4,4,3,3,4]
+arr3 = [1,2,1,2,3,1,1,1,2,2,2]
 
 def count_prime (num):
 	new_num = math.sqrt(num)
@@ -27,10 +29,14 @@ def get_largest(arr):
 			non_prime += 1
 			if non_prime <= prime and a < (len(arr)-1):
 				largest += 1
+			if non_prime > prime:
+				non_prime = 0
+				prime = 0
+				largest = 0
 		if num_type == 'prime':
 			prime += 1
 			largest += 1
 	return largest
 	
-print(get_largest(arr1))
+print(get_largest(arr3))
 			

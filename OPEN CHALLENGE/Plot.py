f8f7095e44ce4acc7da74643126a36a54c02603a
Anil Khatri <anil.soccer.khatri@gmail.com>
2018-10-18 00:49:56 +0530

    Added Sine/Cosine Ploting using Matplotlib
    Environment Setup:
    ``` pip install numpy```
    ```pip install matplotlib```

---
+++ OPEN CHALLENGE/Plot.py
@@ -0,0 +1,24 @@
+# Ploting using matplotlib
+# by - @imkaka
+
+# First Open Terminal/cmd and install:
+# pip install numpy
+# pip install matplotlib
+
+#Imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Compute the x and y coordinates for points on sine and cosine curves
+x = np.arange(0, 3 * np.pi, 0.1)
+y_sin = np.sin(x)
+y_cos = np.cos(x)
+
+# Plot the points using matplotlib
+plt.plot(x, y_sin)
+plt.plot(x, y_cos)
+plt.xlabel('x axis label')
+plt.ylabel('y axis label')
+plt.title('Sine and Cosine')
+plt.legend(['Sine', 'Cosine'])
+plt.show()

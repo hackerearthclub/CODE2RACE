ac96166098e8d1f00befe7ce66876c533c806fdc
SAREESH VATTIAN <sareesh@SAREESHs-MacBook-Pro.local>
2018-10-18 23:50:12 -0700

    made the script simple

--- SOLUTIONS/reverse-word.js
+++ SOLUTIONS/reverse-word.js
@@ -1,11 +1,6 @@
 function reverse_word(input) {
-    let words = input.split(' ');
-    let reverse_word = words.reverse();
-    let word = reverse_word[0];
-    for (let index = 1; index < reverse_word.length; index++) {
-        word = word + ' ' + reverse_word[index];        
-    }
-    console.log(word); 
+    const reverse_word = input.split(' ').reverse().join(' ');
+    console.log(reverse_word); 
 }
 
 //reverse_word('My name is Michele')

e9ec45a929a0522a4e4ac4b7d2d579e50bf28c9b
cclauss <cclauss@bluewin.ch>
2018-10-21 11:15:11 +0200

    ASCII2.py: Works on Python and legacy Python

--- SOLUTIONS/ASCII2.py
+++ SOLUTIONS/ASCII2.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+try:
+    input = raw_input  # Python 2
+except:
+    pass               # Python 2
+
 if __name__ == "__main__":
-    a = raw_input("Enter a string : ")
+    a = input("Enter a string : ")
     res = 1
     for i in range(len(a)):
         res = res * ord(a[i])
-    print('ASCII value : ',res)
+    print('ASCII value : {}'.format(res))

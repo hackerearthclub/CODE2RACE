9f39fd23e6c78b222cc3200116c9de0d4fd04ba3
cclauss <cclauss@bluewin.ch>
2018-10-21 11:09:51 +0200

    Typo in variable name: people_amt

--- SOLUTIONS/ATM_SOLUTION.py
+++ SOLUTIONS/ATM_SOLUTION.py
@@ -2,11 +2,11 @@ t = int(input())
 
 for _ in range(t):
     n,total_amt = [int(v) for v in input().split()]
-    peope_amt = [int(v) for v in input().split()]
+    people_amt = [int(v) for v in input().split()]
     result=""
-    for i in range(len(peope_amt)):
-            if(total_amt>=peope_amt[i]):
-                total_amt=total_amt-peope_amt[i]
+    for i in range(len(people_amt)):
+            if(total_amt>=people_amt[i]):
+                total_amt=total_amt-people_amt[i]
                 result=result+"1"
             else:
                 result+="0"

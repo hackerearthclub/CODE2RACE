1d1800c8ac3e8ebcdd292d6b26ed9e822643b279
cclauss <cclauss@bluewin.ch>
2018-10-21 11:28:57 +0200

    lcm1.py: avoid name collision between variable 'lcm' and function 'find_lcm()'
    Discovered via #336

--- SOLUTIONS/lcm1.py
+++ SOLUTIONS/lcm1.py
@@ -1,4 +1,4 @@
-def lcm(num1,num2):
+def find_lcm(num1,num2):
   if(num1>num2):
     num=num1
     den=num2
@@ -16,7 +16,7 @@ def lcm(num1,num2):
 l=[2,3,4,5,6]
 num1=l[0]
 num2=l[1]
-lcm=lcm(num1,num2)
+lcm=find_lcm(num1,num2)
 for i in range(2,len(l)):
   lcm=find_lcm(num1,num2)
 print(lcm)

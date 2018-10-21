3abda72b351988ec5a46a435dbd46916952ffcb0
cclauss <cclauss@bluewin.ch>
2018-10-21 21:10:03 +0200

    1\n6\n20 40 50 30 90 100

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -27,12 +27,12 @@ def test_bottle():
         for entry in it:
             name = entry.name.lower()
             if entry.is_file() and "bottle" in name and os.path.splitext(name)[-1] == ".py":
-                if entry.path == "SOLUTIONS/emptybottles.py":
-                    continue
+                # if entry.path == "SOLUTIONS/emptybottles.py":
+                #    continue
                 print(entry.path)
-                result = subprocess.run(["python3", entry.path], input="1\n6\n20 40 50 90 100",
+                result = subprocess.run(["python3", entry.path], input="1\n6\n20 40 50 30 90 100",
                                         capture_output=True, check=True, text=True)
-                assert "3" in result.stdout, f"{entry.path} 1\n6\n20 40 50 90 100 did not return 3."
+                assert "3" in result.stdout, f"{entry.path} 1\n6\n20 40 50 30 90 100 did not return 3."
 
 
 @pytest.mark.skipif(sys.version_info < (3, 7), reason="requires Python >= 3.7")

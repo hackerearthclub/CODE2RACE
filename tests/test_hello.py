a0b6d0f76b4127bf67c96faec51893fb131cb7ab
cclauss <cclauss@bluewin.ch>
2018-10-21 20:47:56 +0200

    Add test_bottle()

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -22,6 +22,20 @@ def test_ascii():
 
 
 @pytest.mark.skipif(sys.version_info < (3, 7), reason="requires Python >= 3.7")
+def test_bottle():
+    with os.scandir("SOLUTIONS") as it:
+        for entry in it:
+            name = entry.name.lower()
+            if entry.is_file() and "bottle" in name and os.path.splitext(name)[-1] == ".py":
+                if entry.path in ("SOLUTIONS/emptybottles.py", "SOLUTIONS/empty_bottles.py"):
+                    continue
+                print(entry.path)
+                result = subprocess.run(["python3", entry.path], input="1\n6\n20 40 50 90 100",
+                                        capture_output=True, check=True, text=True)
+                assert "3" in result.stdout, f"{entry.path} 1\n6\n20 40 50 90 100 did not return 3."
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires Python >= 3.7")
 def test_hello():
     with os.scandir("SOLUTIONS") as it:
         for entry in it:

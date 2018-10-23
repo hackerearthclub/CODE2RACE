fcd64d7a6712284447abf7fc8d8c236dfd253716
Luca Gouveia <38117857+luca-gouveia@users.noreply.github.com>
2018-10-23 15:29:00 -0300

    Add Solution - Find Sum Square Difference

--- SOLUTIONS/find_sum_square_diff.py
+++ SOLUTIONS/find_sum_square_diff.py
@@ -4,5 +4,7 @@ def sum_square_dif(number):
     out = ((number*(number+1)*(2*number+1))/6) - ((number*(number+1)/2)**2)
     return abs( int(out) )
 
-number = int(input())
-print(sum_square_dif(number))
+t = int(input())
+for i in range(t):
+    number = int(input())
+    print(sum_square_dif(number))

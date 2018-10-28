a50d878580cc355b911117413d4b791f477f9f6c
kmrmohit <39377854+kmrmohit@users.noreply.github.com>
2018-10-28 17:59:20 +0530

    added fast exponentiation
    calculates a^b in O(log(b))

--- SOLUTIONS/NthPowerOfDigit.cpp
+++ SOLUTIONS/NthPowerOfDigit.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+int fastexp(int a,int b)
+{
+    int ans=1;
+    while(b>0)
+    {
+        if(b&1)
+            ans=(ans*a);
+        b>>=1;
+        a=(a*a);
+    }
+    return ans;
+}
 int sumNthPowerOfDigit(int N) {
     int finalSum = 0;
 
     // (N+1)*pow(9,N) is the upper limit of the brute-force search
-    for (int number = 2; number < (N+1)*pow(9,N); number++) {
+    for (int number = 2; number < (N+1)*fastexp(9,N); number++) {
         int sumOfPowers = 0;
 
         //go through each digit, calculate digit^N, and add to sumOfPowers
@@ -14,7 +25,7 @@ int sumNthPowerOfDigit(int N) {
         while (tempNumber > 0) {
             int digit = tempNumber % 10;
             tempNumber /= 10;
-            sumOfPowers += pow(digit,N);
+            sumOfPowers += fastexp(digit,N);
         }
 
         if (sumOfPowers == number) {

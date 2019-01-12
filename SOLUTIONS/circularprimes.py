f8530200b1faec5a2b2a865f7039ab1e29dbf936
Marko in1t3r Pavlovic <marko.shiva.pavlovic@gmail.com>
2019-01-12 15:06:54 +0100

    improved the answer by adding the a bit more expensive computation for sieve so I get all nums in array for faster checking. comments too.

--- SOLUTIONS/circularprimes.py
+++ SOLUTIONS/circularprimes.py
@@ -1,5 +1,5 @@
-n = int(input())
-
+user_input = int(input())
+n = user_input * 10        # I still think its the faster to compute sieve for a one digit higher then to do it other way
 def sieve_for_primes_to(n):
     size = n//2
     sieve = [1]*size
@@ -12,7 +12,7 @@ def sieve_for_primes_to(n):
     return sieve
 
 arr = ([2] + [i*2+1 for i, v in enumerate(sieve_for_primes_to(n+1)) if v and i>0])
-
+# This array is now a sieve of primes up to the user_input * 10
 def reversed_n(num):
     v = str(num)
     ans = ""
@@ -24,14 +24,21 @@ def reversed_n(num):
 
 ansarr = []
 for v in arr:
+    if v > user_input:          # We are looping up to the user_input limit higher values in arr allow us to check whether
+                                # they are prime num by just checking the array instead of expensive computation
+        break
     if v <= 11:
         ansarr.append(v)
     else:
         try:
-            reversed = int(reversed_n(v))
-            if arr.index(reversed) and ansarr.count(v) == 0:
-                ansarr.append(v)
-                ansarr.append(reversed)
-        except (ValueError, IndexError):
+            reversed = int(reversed_n(v))           # we calculate reversed number by doing string slice
+            if arr.index(reversed) and ansarr.count(v) == 0:    # we check for the reversed num being part of prime nums
+                ansarr.append(v)                                # then we append v
+                if ansarr.count(reversed) == 0:                 # if reversed is not already appended we append
+                    ansarr.append(reversed)
+        except (ValueError, IndexError):                        # in case of reversed not being a prime num we get
+                                                                # ValueError from search operation so we just catch
+                                                                # and continue
             continue
-print(sum(ansarr))
+print(ansarr)
+print(sum(ansarr))          # we just sum appended nums and print the sum

a55b7cc849930f4e53d58e38898e8becf504d711
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2019-10-01 17:18:20 +0530

    3rd largest no in unsorted array using JAVA

---
+++ SOLUTIONS/3rd-largest-no-in-unsorted-array.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++)
+		    arr[i] = sc.nextInt();
+		Arrays.sort(arr);
+		System.out.println(arr[2]);
+	}
+}
